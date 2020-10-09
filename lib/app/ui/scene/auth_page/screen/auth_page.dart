c9210550f3796201612ab49376fd9a2690178e2b
Sakorni <ssakorni@gmail.com>
2020-10-09 13:30:21 +0300

    Removed an unused imports

--- lib/app/ui/scene/auth_page/screen/auth_page.dart
+++ lib/app/ui/scene/auth_page/screen/auth_page.dart
@@ -2,11 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gallery_test/app/resources/app_colors.dart';
-import 'package:gallery_test/app/resources/app_strings.dart';
 import 'package:gallery_test/app/ui/scene/auth_page/bloc/auth_bloc.dart';
 import 'package:gallery_test/app/ui/scene/auth_page/widget/fields_screen.dart';
 import 'package:gallery_test/app/ui/scene/auth_page/widget/cancel_button.dart';
-import 'package:gallery_test/app/ui/scene/auth_page/widget/screen_title.dart';
 import 'package:gallery_test/app/ui/scene/home_page/home_page.dart';
 import 'package:gallery_test/data/gateway/auth_mode.dart';
 
